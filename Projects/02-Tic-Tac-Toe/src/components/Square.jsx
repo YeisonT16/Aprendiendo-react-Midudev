c8d9ff9cd1487d87e5e6cfa3239d4b19d5afd378
Yeison Silva <yeisonsilvat06@gmail.com>
2024-09-29 23:40:16 -0500

    Cambio de estilos en el componente Square

--- Projects/02-Tic-Tac-Toe/src/components/Square.jsx
+++ Projects/02-Tic-Tac-Toe/src/components/Square.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 export const Square = ({children, isSelected, updateBoard, index}) => {
 
     const classTurn = isSelected ? 'text-white bg-sky-500/80' : '' //Esta parte del codigo se debe adaptar con tailwindcss
@@ -7,8 +9,15 @@ export const Square = ({children, isSelected, updateBoard, index}) => {
     }
 
     return (
-    <div className={`${classTurn} size-24 border-2 text-4xl grid items-center cursor-pointer font-semibold rounded-lg border-yellow-600`} onClick={handleClick}>
+    <div className={`${classTurn} size-24 border-4 text-4xl grid items-center cursor-pointer font-semibold rounded-lg border-yellow-500`} onClick={handleClick}>
         {children}
     </div>
     )
+}
+
+Square.propTypes = {
+    children: PropTypes.node,
+    isSelected: PropTypes.bool,
+    updateBoard: PropTypes.func,
+    index: PropTypes.number
 }
